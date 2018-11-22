3abd37752d460f5724afa1c0f383b8715dd0bac3
Arturo Filio Villa <arturfil@hotmail.com>
2018-11-22 13:03:35 -0500

    teting navbar css

--- olog2/settings.py
+++ olog2/settings.py
@@ -109,5 +109,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'olog/static')
+    os.path.join(BASE_DIR, 'olog2/static')
 ]
